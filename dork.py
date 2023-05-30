570cbe952e047ba75f259294d0c246a90aca3646
0xCoolAnon <110695357+0xCoolAnon@users.noreply.github.com>
2023-05-30 20:21:38 +0800

    dork.py

--- dork.py
+++ dork.py
@@ -1,5 +1,7 @@
 # CODER: 0xCoolAnon
 # This file have a virus
+
+
 from googlesearch import search
 from colorama import Fore
 import os
@@ -23,7 +25,11 @@ try:
     # Prompt the user for inputs
     ny1 = input(f"{Fore.YELLOW}\n[💀] #OpsHebrew [💀]{Fore.RED} - {Fore.GREEN}Dork: {Fore.CYAN}")
     ny2 = int(input(f"{Fore.YELLOW}\n[💀] #OpsHebrew [💀]{Fore.RED} - {Fore.GREEN}Amount: {Fore.CYAN}"))
-    ny3 = int(input(f"{Fore.YELLOW}\n[💀] #OpsHebrew [💀]{Fore.RED} - {Fore.GREEN}Time (Seconds): {Fore.CYAN}"))
+    ny3 = int(input(f"{Fore.YELLOW}\n[💀] #OpsHebrew [💀]{Fore.RED} - {Fore.GREEN}Time (Minutes): {Fore.CYAN}"))
+
+    # Convert minutes to seconds
+    ny3 = ny3 * 60
+
     custom_file_name = input(f"{Fore.YELLOW}\n[💀] #OpsHebrew [💀]{Fore.RED} - {Fore.GREEN}File Name You Want to Create: {Fore.CYAN}")
     print(f'{Fore.BLUE}\n═════════════════════════════════════════════')
 
@@ -32,13 +38,22 @@ try:
     custom_file_path = os.path.join('result', custom_file_name)
 
     nyx = 0
-    for i in search(ny1, tld="com", lang="en", num=int(ny2), start=0, stop=None, pause=int(ny3)):
-        with open(custom_file_path, 'a') as f:
-            f.write(f'{i}\n')
-        nyx += 1
-        print(f'{Fore.WHITE}       {nyx}) {Fore.CYAN}{i}')
-        if nyx >= int(ny2):
-            break
+    ny2 = 10  # Initial value for the number of results
+    while nyx < ny2:
+        for i in search(ny1, num=ny2, start=nyx, stop=None, pause=ny3):
+            with open(custom_file_path, 'a') as f:
+                f.write(f'{i}\n')
+            nyx += 1
+            print(f'{Fore.WHITE}       {nyx}) {Fore.CYAN}{i}')
+            if nyx >= ny2:
+                break
+
+        if nyx < ny2:
+            prompt = input(f"{Fore.YELLOW}\n[💀] #OpsHebrew [💀]{Fore.RED} - {Fore.GREEN}Continue searching for more results? (Y/N): {Fore.CYAN}")
+            if prompt.lower() != 'y':
+                break
+
+        ny2 += 10  # Increase the number of results by 10 for each iteration
 
     print(f'{Fore.BLUE}═════════════════════════════════════════════')
     print(f'{Fore.YELLOW}\n[D0rK!] {Fore.GREEN}Saved: {Fore.GREEN}{custom_file_path}')
